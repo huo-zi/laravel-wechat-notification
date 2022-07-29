7a1cad37a302ea1b09eb834722c6cdbf90491c58
lio990527 <lio990527@163.com>
2022-07-29 13:39:46 +0800

    使用默认配置

--- src/WechatMessage.php
+++ src/WechatMessage.php
@@ -9,27 +9,27 @@ use Huozi\LaravelWechatNotification\Messages\WechatWorkMessage;
 class WechatMessage
 {
 
-    public static function officialAccount($name = null)
+    public static function officialAccount($name = '')
     {
         return new OfficateAccountTemplateMessage(Facade::officialAccount($name));
     }
 
-    public static function miniProgram($name = null)
+    public static function miniProgram($name = '')
     {
         return new MiniProgramTemplateMessage(Facade::miniProgram($name));
     }
 
-    public static function openFlatform($name = null)
+    public static function openFlatform($name = '')
     {
         return new WechatPlatform(Facade::openPlatform($name));
     }
 
-    public static function work($name)
+    public static function work($name = '')
     {
         return new WechatWorkMessage(Facade::work($name)->message);
     }
 
-    public static function openWork($name)
+    public static function openWork($name = '')
     {
         return new WechatOpenWork($name ? app('wechat.open_work.'.$name) : app('wechat.open_work'));
     }
